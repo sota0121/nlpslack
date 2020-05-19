41ed03276c219100e749ca5b82eb5c7e21721415
sota_masuda <masuda.kyushu@gmail.com>
2020-05-19 11:51:54 +0900

    connect test to bq with service account auth

--- nlpslack/main.py
+++ nlpslack/main.py
@@ -1,6 +1,9 @@
+import os
 import json
 from pathlib import Path
 import slackapp as sa
+from google.cloud import bigquery
+
 
 
 CREDENTIALS_PATH = '../data/conf/credentials.json'
@@ -25,7 +28,25 @@ def slack_msg_extraction(credentials_path: str, outdir: str) -> bool:
     app.load_save_messages_info(outdir)
     return True
 
+
 # make message table with db and preprocessing
+def bq_test():
+    script_dir = str(Path(__file__).resolve().parent)
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = script_dir + '/../data/conf/service_account.json'
+    
+    client = bigquery.Client()
+
+    # Perform a query.
+    QUERY = (
+        'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
+        'WHERE state = "TX" '
+        'LIMIT 10')
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+
+    for row in rows:
+        print(row.name)
+
 # cleaning with preprocessing
 # morphological_analysis  with preprocessing
 # normalization  with preprocessing
@@ -35,8 +56,9 @@ def slack_msg_extraction(credentials_path: str, outdir: str) -> bool:
 
 
 def main():
-    ret = slack_msg_extraction(CREDENTIALS_PATH, RAWDATA_PATH)
-    print(ret)
+    #ret = slack_msg_extraction(CREDENTIALS_PATH, RAWDATA_PATH)
+    #print(ret)
+    bq_test()
 
 
 if __name__ == "__main__":
