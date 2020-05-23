104a1e67bfadc4dcda7b4e72721a0c21d2cf7fb1
sota_masuda <masuda.kyushu@gmail.com>
2020-05-23 21:56:46 +0900

    add learned w2v model with wiki dataset

---
+++ data/model/model.py
@@ -0,0 +1,15 @@
+from gensim.models import word2vec
+from pathlib import Path
+import logging
+
+SCRIPT_DIR = str(Path(__file__).resolve().parent) + '/'
+DATASET_DIR = SCRIPT_DIR + 'dataset/'
+WIKI_DATASET_NAME = 'wiki_wakati.txt'
+WIKI_MODEL_NAME = 'wiki.model'
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
+sentences = word2vec.Text8Corpus(DATASET_DIR + WIKI_DATASET_NAME)
+
+model = word2vec.Word2Vec(sentences, size=200, min_count=20, window=15)
+model.save("./" + WIKI_MODEL_NAME)
